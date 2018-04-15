8d9d0c84fd10aea6fce76152369d0bd8ae200006
Paresh Nayyar <nayyar.paresh@gmail.com>
2018-04-15 18:04:17 +0530

    changed source from google to yahoo

--- analysis.R
+++ analysis.R
@@ -5,8 +5,8 @@ end   <- as.Date("2017-04-23")
 
 #xts objects (provided in the xts package) are seen as improved versions of the ts object
 #for storing time series data.
-getSymbols("AAPL", src = "google", from = start, to = end)
-getSymbols("GOOGL" , src = "google" , from = start , to = end)
+getSymbols("AAPL", src = "yahoo", from = start, to = end)
+getSymbols("GOOGL" , src = "yahoo" , from = start , to = end)
 
 par(mfrow=c(2,1))
 plot(AAPL[,"AAPL.Open"],main = "AAPL Open")
@@ -14,7 +14,7 @@ plot(AAPL[,"AAPL.High"],main = "AAPL High")
 plot(AAPL[,"AAPL.Low"],main = "AAPL Low")
 plot(AAPL[,"AAPL.Close"],main = "AAPL Close")
 plot(AAPL[,"AAPL.Volume"],main = "Volume Traded Apple")
-candleChart(AAPL, up.col = "black", dn.col = "red", theme = "white")
+candleChart(AAPL, up.col = "green", dn.col = "red", theme = "white")
 
 plot(GOOGL[,"GOOGL.Open"],main = "GOOGL Open")
 plot(GOOGL[,"GOOGL.High"],main = "GOOGL High")
