f5ded5708cf422809759a09a7966675ced5dd1b2
Paresh Nayyar <nayyar.paresh@gmail.com>
2018-04-17 07:35:27 +0530

    updated stock return plot

--- plottingTS.R
+++ plottingTS.R
@@ -40,9 +40,9 @@ plot_grid(g1, g2, labels= NULL, ncol = 1, nrow = 2)
 
 # PLotting with two different scale
 par(mar = c(5,5,2,5))
-plot(apple$Opening_Rate,type = 'l',col='red3',ylab = 'Apple Opening Price')
+plot(apple$Closing_Rate,type = 'l',col='red3',ylab = 'Apple Opening Price')
 par(new=T)
-plot(google$Opening_Rate,type = 'l',col='blue',xlab = NA,ylab = NA,axes = F)
+plot(google$Closing_Rate,type = 'l',col='blue',xlab = NA,ylab = NA,axes = F)
 axis(side=4) #pu the axis on right side
 mtext(side = 4,line = 3,'Google Opening Price')
 legend('topleft',legend = c('Apple','Google'),col=c('red3','blue'),lty = c(1,1),cex = 0.8)
@@ -71,5 +71,14 @@ dygraph(mat , main = 'Apple Stock' , xlab = 'Date' , ylab = 'Stock Price')  %>%
       dyRangeSelector()
 
 # Plot Returns
-getstocks <- as.matrix(cbind('Apple.Close'=apple$Closing_Rate,'Google.Close'=google$Closing_Rate))
-stock_return <- apply(getstocks,1,function(x) {x/getstocks[1,]}) %>% t 
+getstocks <- as.xts(data.frame(AAPL[,'AAPL.Close'],GOOGL[,'GOOGL.Close']))
+names(getstocks) <- c('Apple','Google')
+stock_return <- apply(getstocks,1,function(x) {x/getstocks[1,]}) %>% t %>% as.xts
+dygraph(stock_return,ylab='Return',
+        main='Stocks Return') %>%
+        dySeries("Apple",label='Apple') %>%
+        dySeries("Google",label = 'Google') %>%
+        dyOptions(colors = c('brown','blue'))
+        dyRangeSelector() %>%
+        dyOptions(strokeWidth = 3)
+#plot.xts(stock_return,ylab='Return',col=c('brown','blue'),legend.loc = 'topleft')
